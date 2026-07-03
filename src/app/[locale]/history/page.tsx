02d26eff9d0227335272c3d5e371306e6da15b9c
Nastya Belova <nastjabelova0@gmail.com>
2026-07-03 03:54:11 +0300

    feat: add proxy.ts with routes protection

--- src/app/[locale]/history/page.tsx
+++ src/app/[locale]/history/page.tsx
@@ -1,15 +1,4 @@
-import { redirect } from 'next/navigation';
-
-import { createClient } from '@/utils/supabase/client';
-
 const HistoryPage = async () => {
-  const supabaseClient = createClient();
-  const {
-    data: { user },
-  } = await supabaseClient.auth.getUser();
-  if (!user) {
-    redirect('/');
-  }
   return <div>History Page</div>;
 };
 
--- src/proxy.ts
+++ src/proxy.ts
@@ -2,11 +2,32 @@ import { NextRequest } from 'next/server';
 import createMiddleware from 'next-intl/middleware';
 
 import { routing } from './i18n/routing';
+import { updateSession } from './utils/supabase/proxy';
 
 const handleI18nRouting = createMiddleware(routing);
 
-export function proxy(req: NextRequest) {
-  return handleI18nRouting(req);
+export async function proxy(req: NextRequest) {
+  const response = handleI18nRouting(req);
+
+  // const pathname = req.nextUrl.pathname;
+
+  // if (pathname === '/' || pathname === '/ru' || pathname === '/en') {
+  //   return handleI18nRouting(req);
+  // }
+  const supabaseResponse = await updateSession(req);
+
+  // if (
+  //   supabaseResponse.headers.get('x-middleware-redirect') ||
+  //   (supabaseResponse.status >= 300 && supabaseResponse.status < 400)
+  // ) {
+  //   return supabaseResponse;
+  // }
+
+  // supabaseResponse.cookies.getAll().forEach((cookie) => {
+  //   response.cookies.set(cookie.name, cookie.value);
+  // });
+
+  return response;
 }
 
 export const config = {
