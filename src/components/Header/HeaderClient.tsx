80c940d55825a5ec29ba2e1d677e1080fc4b5263
Nastya Belova <nastjabelova0@gmail.com>
2026-07-06 15:43:42 +0300

    refactor: improve header code and logic

--- src/components/Header/HeaderClient.tsx
+++ src/components/Header/HeaderClient.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { User } from '@supabase/supabase-js';
-import { usePathname } from 'next/navigation';
+import { useParams, usePathname } from 'next/navigation';
 
 import { Link } from '@/i18n/navigation';
 
@@ -14,38 +14,35 @@ export type HeaderProps = {
 
 const HeaderClient: React.FC<HeaderProps> = ({ user }) => {
   const pathname = usePathname();
+  const params = useParams();
+  const locale = params?.locale;
+
   const isAbout = pathname?.includes('/about');
   const isHistory = pathname?.includes('/history');
+  const isHome = pathname === '/' || pathname === `/${locale}`;
+
   return (
     <header className={styles.header}>
       <nav className={styles.headerNav}>
         <div className={styles.navLeft}>
-          {isAbout ? (
-            <Link className={styles.link} href="/">
-              Home
-            </Link>
-          ) : (
-            <Link className={styles.link} href="/about">
+          {!isAbout && (
+            <Link className={styles.link} href={`/about`}>
               About
+            </Link>
+          )}
+          {!isHome && (
+            <Link className={styles.link} href={`/`}>
+              Home
             </Link>
           )}
         </div>
         <div className={styles.navRight}>
-          {user === null ? (
-            <></>
-          ) : (
-            <>
-              {isHistory ? (
-                <Link className={styles.link} href="/">
-                  Home
-                </Link>
-              ) : (
-                <Link className={styles.link} href="/history">
-                  History
-                </Link>
-              )}
-            </>
+          {user && !isHistory && (
+            <Link className={styles.link} href={`/history`}>
+              History
+            </Link>
           )}
+
           <AuthComponent user={user}></AuthComponent>
         </div>
       </nav>
